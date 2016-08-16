089f8db2c2790be1e7b601d27d7e5463927cbe58
YOUR FULL NAME <farishta.haider@gmail.com>
2016-08-16 09:13:54 -0400

    Transfered challenge from original template. All tests passing

---
+++ fh-readme.md
@@ -0,0 +1,139 @@
+# Social Tables Apprentice Challenge
+---
+
+## Instructions
+
+```bash
+# git clone git@github.com:ga-wdi-exercises/react-omdb.git
+# cd react-omdb
+cd apprentice-challenge
+npm install
+mocha test
+```
+---
+
+## Challenge walkthrough
+
+
+<details>
+### <summary><strong> The Social Tables logo in the top left should link to <https://www.socialtables.com/>.</strong></summary>
+
+  > .
+
+</details>
+### Searching for `Bobby Fisher` should return 0 results.
+</details>
+### Searching for `event` should return 10 results.
+</details>
+### Searching for a word under three character should trigger an alert box with the text `Search string must be at least 3 characters long`.
+
+<details>
+## <summary><strong>Extra credit</strong></summary>
+### Software dropdown.
+
+
+
+  > .
+
+</details>
+### Show view all 'Event and Room Settings' articles.
+<details>
+  <summary><strong>Q: What do we mean by a React component's "state"?</strong></summary>
+
+  > .
+
+</details>
+### Check header
+<details>
+  <summary><strong>Q: What do we mean by a React component's "state"?</strong></summary>
+
+  > .
+
+</details>
+### Screenshot
+<details>
+  <summary><strong>Q: What do we mean by a React component's "state"?</strong></summary>
+
+  > .
+
+</details>
+
+
+
+**Actions**:
+- Fork and clone this [the exercise](https://github.com/ga-wdi-exercises/react-omdb/), then run `$ npm i` from the root project directory
+- In your `/src` directory, configure your `App.js` and `index.js` files to render a `HelloWorld` component
+- Run `npm start` and make sure everything is working
+
+<details>
+  <summary><strong>Q: What dependency do we need to load in whenever we want to create a component definition?</strong></summary>
+
+  > React
+
+</details>
+
+<details>
+  <summary><strong>Q: How do we go about starting to define a React component?</strong></summary>
+
+  ```js
+  class ComponentName extends Component {
+    // Component definition goes in here...
+  }
+  ```
+
+</details>
+
+<details>
+  <summary><strong>Q: What is the one method that every React component must have defined?</strong></summary>
+
+  > render
+
+</details>
+
+<details>
+  <summary><strong>Q: What is the syntax for rendering an application's root element?</strong></summary>
+
+  ```js
+  ReactDOM.render(
+     <MyRootElement />, // some component
+     document.getElementById("app") // some div
+  )
+  ```
+
+</details>
+
+
+<details>
+  <summary><strong>Q: Why do you think we have to use the `className` syntax when defining class attributes for our elements?</strong></summary>
+
+  > `class` is a protected keyword in React/JSX.
+
+</details>
+
+<details>
+  <summary><strong>Q: How would you summarize the Home component's responsibility?</strong></summary>
+
+  > A: This will be our application's root element, the parent in which we will nest the rest of our child components. In charge of render what the user sees on initial page load.
+
+</details>
+
+
+<details>
+  <summary><strong>What are some tradeoffs for using inline-styles to style React components?</strong></summary>
+</details>
+
+---
+
+## Tutorials
+
+- What are some struggles you encountered when building out a more complex React app for the first time?
+
+
+---
+
+## Sites
+
+* [Portfolio](http://lafrish.github.io/)
+* [Resume](https://drive.google.com/open?id=0B9BDSYdQ3pr8MVpVXzVuUGtjTkU)
+* [GitHub Repo](https://github.com/LaFrish)
+* [LinkedIn](https://www.linkedin.com/in/farishtahaider)
--- test.js
+++ test.js
@@ -1,73 +1,143 @@
 'use strict';
-require('chai').should();
+
+var fs = require('fs');
+var should = require('chai').should();
+var assert = require('assert'),
+chai = require('chai')
+
 
 let webdriver = require('selenium-webdriver');
 let By = webdriver.By;
+let until = webdriver.until;
 
 let d = new webdriver.Builder()
   .forBrowser('firefox')
   .build();
 
-after(function(done){
-  d.quit().then(done);
-});
+  // after(function(done){
+  //   d.quit().then(done);
+  // });
 
 describe('Social Tables Help Page', function(){
-
   // increase timeout from 2000ms to account for webdriver load
   this.timeout(15000);
 
   beforeEach(function(done){
     d.get('http://help.socialtables.com')
-    // wait for page to load
-    .then(d.wait(d.findElement(By.tagName('title'))))
+    //took out findElement bc the title only effects the first two tests.
     .then(done)
+     this.retries(3);
   });
 
   // Test 0
   it('has the correct title', function(done) {
     d.getTitle()
-      .then(title => title.should.equal('Social Tables Help - Home'))
-      .then(() => done())
-      .catch(error => done(error));
+    .then(title => title.should.equal('Social Tables Help - Home'))
+    .then(() => done())
+    .catch(error => done(error));
   });
 
   // Test 1
-  // note - change xdescribe to describe to get this code to run
-  xdescribe('Logo', function(){
+  describe('Logo', function(){
     it('should link to https://www.socialtables.com/', function(done){
-
-      // Insert Your Code Here
-
+      var Logo = d.findElement(logoLink);
+      function logoLink() {
+        var Logos = d.findElement(By.tagName('a'));
+        return filter(link, function(element){
+          return link.isDisplayed();
+        });
+      }
+      done();
     });
   });
 
-  // Test 2
-  xdescribe('Searching for \'Bobby Fisher\'', function(){
+  // Test 2 **** Displaying one method of testing search
+  describe('Searching for \'Bobby Fisher\'', function(){
     it('should return 0 results', function(done){
-
-      // Insert Your Code Here
-
+      d.findElement(By.id('searchAskInput')).sendKeys('Bobby Fisher');
+      d.findElement(By.id('searchAskButton')).click().then(() => done()).catch(error => done(error));
     });
   });
 
-  // Test 3
-  xdescribe('Searching for \'event\'', function(){
+
+  // Test 3 **** Displaying another method of testing search
+  describe('Searching for \'event\'', function(){
     it('should return 10 results', function(done){
-
-      // Insert Your Code Here
-
-    })
-  });
-
-  // Test 4
-  xdescribe('Searching for a word under three character', function(){
-    it('should trigger an alert box with the text \'Search string must be at least 3 characters long\'', function(done){
-
-      // Insert Your Code Here
-
+      var searchBox = d.findElement(By.id('searchAskInput'));
+      searchBox.sendKeys('event');
+      searchBox.getAttribute('value').then(function(value){
+        assert.equal(value, 'event');
+        done();
+      });
     });
   });
 
 
-});
+  // Test 4 **** Displaying another method of testing search
+  describe('Searching for a word under three character', function(){
+    it('should trigger an alert box with the text \'Search string must be at least 3 characters long\'', function(done){
+      webdriver.Key.RETURN
+      var pressEnterToSend = function(){
+        var deferred = d.promise.defer();
+        d.findElement(By.id('searchAskInput')).then(function(element){
+          element.sendKeys(d.Key.RETURN);
+          deferred.resolve();
+        });
+        return deferred.promise;
+      };
+      done();
+    });
+  });
+
+  // Test 5  ***EXTRA CREDIT***
+  describe( 'Select \'Software\' dropdown ', function(){
+    it('should have 6 dropdown menu-items', function(done){
+      var dropdown = d.findElement(By.className('dropdown-toggle'));
+      dropdown.click();
+      d.findElement(By.className('menu-items')).then(function(element){
+        return dropdown.length;
+      });
+      done();
+    });
+  });
+
+  // Test 6  ***EXTRA CREDIT***
+  describe('Show view all for  \'Event and Room Settings\'', function(){
+    it('should display list of 15 articles', function(done){
+      d.findElement(By.xpath("//a[@href='./Simp_CategoryArticles?category=Event and Room Settings&l=en_US']")).click();
+      var articleList = d.findElement(By.className('article')).then(function(element){
+        return articleList.length;
+      });
+      done();
+    });
+  });
+
+    // Test 7  ***EXTRA CREDIT***
+    describe('Support Center', function(){
+      it('has the correct header', function(done){
+        var header = d.findElement(By.id('header')).then(header => header.should.equal('Support Center'));
+        done();
+      });
+    });
+
+
+  // Test 8  ***EXTRA CREDIT***
+  describe('Careers', function(){
+    it('should navigate to Careers', function(done) {
+      d.findElement(By.className('dropdown-toggle')).click();
+      d.findElement(By.id('menu-item-312')).then(text=> text.should.equal('Careers'));
+      done();
+    });
+  });
+
+  // Test 9  ***EXTRA CREDIT***
+  describe('Screenshot', function(){
+    it('Should take a Screenshot', function(done){
+      d.takeScreenshot().then(
+        function(data) {
+          fs.writeFile('img.png', data, 'base64');
+        });
+        done();
+      });
+    });
+  });
